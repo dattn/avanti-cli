b8b3770b0e5ecac357c3eb0d37d78ab07dcd533d
Daniel Duton <daniel@duton.lu>
2017-06-05 10:55:47 +0200

    add inquirer host check from core

--- src/client/remove.js
+++ src/client/remove.js
@@ -1,5 +1,6 @@
 import Client from 'avanti-core/dist/client';
 import chalk from 'chalk';
+import inquirer from 'inquirer';
 
 export const execute = async (options) => {
     if (!options.client) {
@@ -9,7 +10,24 @@ export const execute = async (options) => {
     }
 
     try {
-        await (new Client(options.client)).remove();
+        let client = await Client.get(options.client);
+
+        let hosts = await client.hosts();
+        if (hosts.length) {
+            process.stdout.write('The following hosts will be deleted:\n');
+            process.stdout.write(hosts.join(', ') + '\n');
+            let { proceed } = await inquirer.prompt([{
+                name: 'proceed',
+                type: 'confirm',
+                message: 'Are you sure you want to proceed?',
+                default: false
+            }]);
+            if (!proceed) {
+                return;
+            }
+        }
+
+        await client.remove();
     } catch(e) {
         process.exitCode = 1;
         process.stderr.write(chalk.red(chalk.bold('ERROR:') + ' ' + e) + '\n');
